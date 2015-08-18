c24f23668e94973aa37bea8e398f51d5a419c5f2
yzyan <yyuzhong@hotmail.com>
2015-08-18 10:54:07 -0500

    [YZ] setup env, change script

---
+++ src/ipf/display.py
@@ -0,0 +1,229 @@
+"""
+Demonstrate 3D seismic image processing for faults and horizons
+Author: Dave Hale, Colorado School of Mines
+Version: 2014.07.17
+"""
+
+from fakeutils import *
+s1,s2,s3 = getSamplings()
+n1,n2,n3 = s1.count,s2.count,s3.count
+
+# Names and descriptions of image files used below.
+gxfile  = "gx" # input image (maybe after bilateral filtering)
+gsxfile = "gsx" # image after lsf with fault likelihoods
+epfile  = "ep" # eigenvalue-derived planarity
+p2file  = "p2" # inline slopes
+p3file  = "p3" # crossline slopes
+p2kfile = "p2k" # inline slopes (known)
+p3kfile = "p3k" # crossline slopes (known)
+flfile  = "fl" # fault likelihood
+fpfile  = "fp" # fault strike (phi)
+ftfile  = "ft" # fault dip (theta)
+fltfile = "flt" # fault likelihood thinned
+fptfile = "fpt" # fault strike thinned
+fttfile = "ftt" # fault dip thinned
+fs1file = "fs1" # fault slip (1st component)
+fs2file = "fs2" # fault slip (2nd component)
+fs3file = "fs3" # fault slip (3rd component)
+fskbase = "fsk" # fault skin (basename only)
+
+# These parameters control the scan over fault strikes and dips.
+# See the class FaultScanner for more information.
+minPhi,maxPhi = 0,360
+minTheta,maxTheta = 65,85
+sigmaPhi,sigmaTheta = 4,20
+
+# These parameters control the construction of fault skins.
+# See the class FaultSkinner for more information.
+lowerLikelihood = 0.2
+upperLikelihood = 0.5
+minSkinSize = 3000
+
+# These parameters control the computation of fault dip slips.
+# See the class FaultSlipper for more information.
+minThrow = 0.01
+maxThrow = 15.0
+
+# Directory for saved png images. If None, png images will not be saved;
+# otherwise, must create the specified directory before running this script.
+pngDir = None
+#pngDir = "../../png/"
+
+# Processing begins here. When experimenting with one part of this demo, we
+# can comment out earlier parts that have already written results to files.
+def main(args):
+  goDisplay(args)
+
+def goDisplay(args):
+  gx = readImage(args[1])
+  impedance = False # if True, data = impedance model
+  print "gx min =",min(gx)," max =",max(gx)
+  gmin,gmax,gmap = -3.0,3.0,ColorMap.GRAY
+  if impedance:
+    gmin,gmax,gmap = 0.0,1.4,ColorMap.JET
+  plot3(gx,cmin=gmin,cmax=gmax,cmap=gmap,clab="Amplitude",png="gx")
+
+def jetFill(alpha):
+  return ColorMap.setAlpha(ColorMap.JET,alpha)
+def jetFillExceptMin(alpha):
+  a = fillfloat(alpha,256)
+  a[0] = 0.0
+  return ColorMap.setAlpha(ColorMap.JET,a)
+def jetRamp(alpha):
+  return ColorMap.setAlpha(ColorMap.JET,rampfloat(0.0,alpha/256,256))
+def bwrFill(alpha):
+  return ColorMap.setAlpha(ColorMap.BLUE_WHITE_RED,alpha)
+def bwrNotch(alpha):
+  a = zerofloat(256)
+  for i in range(len(a)):
+    if i<128:
+      a[i] = alpha*(128.0-i)/128.0
+    else:
+      a[i] = alpha*(i-127.0)/128.0
+  return ColorMap.setAlpha(ColorMap.BLUE_WHITE_RED,a)
+def hueFill(alpha):
+  return ColorMap.getHue(0.0,1.0,alpha)
+def hueFillExceptMin(alpha):
+  a = fillfloat(alpha,256)
+  a[0] = 0.0
+  return ColorMap.setAlpha(ColorMap.getHue(0.0,1.0),a)
+
+def addColorBar(frame,clab=None,cint=None):
+  cbar = ColorBar(clab)
+  if cint:
+    cbar.setInterval(cint)
+  cbar.setFont(Font("Arial",Font.PLAIN,32)) # size by experimenting
+  cbar.setWidthMinimum
+  cbar.setBackground(Color.WHITE)
+  frame.add(cbar,BorderLayout.EAST)
+  return cbar
+
+def convertDips(ft):
+  return FaultScanner.convertDips(0.2,ft) # 5:1 vertical exaggeration
+
+def plot3(f,g=None,cmin=None,cmax=None,cmap=None,clab=None,cint=None,
+          xyz=None,cells=None,skins=None,smax=0.0,
+          links=False,curve=False,trace=False,png=None):
+  n1 = len(f[0][0])
+  n2 = len(f[0])
+  n3 = len(f)
+  sf = SimpleFrame(AxesOrientation.XRIGHT_YOUT_ZDOWN)
+  cbar = None
+  if g==None:
+    ipg = sf.addImagePanels(s1,s2,s3,f)
+    if cmap!=None:
+      ipg.setColorModel(cmap)
+    if cmin!=None and cmax!=None:
+      ipg.setClips(cmin,cmax)
+    else:
+      ipg.setClips(-3.0,3.0)
+    if clab:
+      cbar = addColorBar(sf,clab,cint)
+      ipg.addColorMapListener(cbar)
+  else:
+    ipg = ImagePanelGroup2(s1,s2,s3,f,g)
+    ipg.setClips1(-3.0,3.0)
+    if cmin!=None and cmax!=None:
+      ipg.setClips2(cmin,cmax)
+    if cmap==None:
+      cmap = jetFill(0.8)
+    ipg.setColorModel2(cmap)
+    if clab:
+      cbar = addColorBar(sf,clab,cint)
+      ipg.addColorMap2Listener(cbar)
+    sf.world.addChild(ipg)
+  if cbar:
+    cbar.setWidthMinimum(120)
+  if xyz:
+    pg = PointGroup(0.2,xyz)
+    ss = StateSet()
+    cs = ColorState()
+    cs.setColor(Color.YELLOW)
+    ss.add(cs)
+    pg.setStates(ss)
+    #ss = StateSet()
+    #ps = PointState()
+    #ps.setSize(5.0)
+    #ss.add(ps)
+    #pg.setStates(ss)
+    sf.world.addChild(pg)
+  if cells:
+    ss = StateSet()
+    lms = LightModelState()
+    lms.setTwoSide(True)
+    ss.add(lms)
+    ms = MaterialState()
+    ms.setSpecular(Color.GRAY)
+    ms.setShininess(100.0)
+    ms.setColorMaterial(GL_AMBIENT_AND_DIFFUSE)
+    ms.setEmissiveBack(Color(0.0,0.0,0.5))
+    ss.add(ms)
+    cmap = ColorMap(0.0,1.0,ColorMap.JET)
+    xyz,uvw,rgb = FaultCell.getXyzUvwRgbForLikelihood(0.5,cmap,cells,False)
+    qg = QuadGroup(xyz,uvw,rgb)
+    qg.setStates(ss)
+    sf.world.addChild(qg)
+  if skins:
+    sg = Group()
+    ss = StateSet()
+    lms = LightModelState()
+    lms.setTwoSide(True)
+    ss.add(lms)
+    ms = MaterialState()
+    ms.setSpecular(Color.GRAY)
+    ms.setShininess(100.0)
+    ms.setColorMaterial(GL_AMBIENT_AND_DIFFUSE)
+    if not smax:
+      ms.setEmissiveBack(Color(0.0,0.0,0.5))
+    ss.add(ms)
+    sg.setStates(ss)
+    size = 2.0
+    if links:
+      size = 0.5 
+    for skin in skins:
+      if smax>0.0: # show fault throws
+        cmap = ColorMap(0.0,smax,ColorMap.JET)
+        xyz,uvw,rgb = skin.getCellXyzUvwRgbForThrow(size,cmap,False)
+      else: # show fault likelihood
+        cmap = ColorMap(0.0,1.0,ColorMap.JET)
+        xyz,uvw,rgb = skin.getCellXyzUvwRgbForLikelihood(size,cmap,False)
+      qg = QuadGroup(xyz,uvw,rgb)
+      qg.setStates(None)
+      sg.addChild(qg)
+      if curve or trace:
+        cell = skin.getCellNearestCentroid()
+        if curve:
+          xyz = cell.getFaultCurveXyz()
+          pg = PointGroup(0.5,xyz)
+          sg.addChild(pg)
+        if trace:
+          xyz = cell.getFaultTraceXyz()
+          pg = PointGroup(0.5,xyz)
+          sg.addChild(pg)
+      if links:
+        xyz = skin.getCellLinksXyz()
+        lg = LineGroup(xyz)
+        sg.addChild(lg)
+    sf.world.addChild(sg)
+  ipg.setSlices(95,5,51)
+  #ipg.setSlices(95,5,95)
+  if cbar:
+    sf.setSize(837,700)
+  else:
+    sf.setSize(700,700)
+  vc = sf.getViewCanvas()
+  vc.setBackground(Color.WHITE)
+  radius = 0.5*sqrt(n1*n1+n2*n2+n3*n3)
+  ov = sf.getOrbitView()
+  ov.setWorldSphere(BoundingSphere(0.5*n1,0.5*n2,0.5*n3,radius))
+  ov.setAzimuthAndElevation(-55.0,25.0)
+  ov.setTranslate(Vector3(0.0241,0.0517,0.0103))
+  ov.setScale(1.2)
+  sf.setVisible(True)
+  if png and pngDir:
+    sf.paintToFile(pngDir+png+".png")
+    if cbar:
+      cbar.paintToPng(137,1,pngDir+png+"cbar.png")
+
+#############################################################################
+run(main)
